afd276a47a7512b6a2e3d0656e3202e5213bc71b
kiki <christianjuric187@gmail.com>
2026-04-03 11:51:08 +0200

    Implemented nvim file tree

--- lua/kiki/plugins/editor.lua
+++ lua/kiki/plugins/editor.lua
@@ -43,6 +43,57 @@ return {
     },
   },
 
+  -- file explorer (nvim-tree)
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>ee", "<cmd>NvimTreeToggle<CR>",   desc = "File tree toggle" },
+      { "<leader>ef", "<cmd>NvimTreeFindFile<CR>", desc = "Find tree find file" },
+    },
+
+    opts = {
+      on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
+        local function map(lhs, rhs, desc)
+          vim.keymap.set("n", lhs, rhs, { buffer = bufnr, silent = true, nowait = true, desc = desc })
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+
+        map("l",     api.node.open.edit,             "Open")
+        map("h",     api.node.navigate.parent_close, "Close dir")
+        map("<C-v>", api.node.open.vertical,         "Open vertical")
+        map("<C-x>", api.node.open.horizontal,       "Open horizontal")
+      end,
+
+      view = {
+        width = 35,
+        side  = "left",
+        preserve_window_proportions = true,
+      },
+      renderer = {
+        highlight_git = true,
+        highlight_opened_files = "name",
+        indent_markers = { enable = false },
+      },
+      filters = {
+        dotfiles = false,
+      },
+      git = { enable = true, ignore = false },
+      actions = {
+        open_file = {
+          quit_on_open = false,
+          window_picker = { enable = true },
+        },
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+      },
+    },
+  },
+
   -- utility
   { "nvim-lua/plenary.nvim", lazy = true },
 }
