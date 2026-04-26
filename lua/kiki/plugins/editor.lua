1fc6be53978308ab0df531b9e833c4560d73bf02
kiki <christianjuric187@gmail.com>
2026-04-26 22:09:21 +0200

    Implement fuzzy finder skeleton

--- lua/kiki/plugins/editor.lua
+++ lua/kiki/plugins/editor.lua
@@ -94,6 +94,24 @@ return {
     },
   },
 
+  -- fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      local builtin = require "telescope.builtin"
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end,
+  },
+
   -- utility
   { "nvim-lua/plenary.nvim", lazy = true },
 }
